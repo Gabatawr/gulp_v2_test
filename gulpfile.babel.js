710134bf9d45f471aa645cf973d414396024b543
Gabatawr <gabatawr@gmail.com>
2021-06-06 18:58:58 +0300

    add

---
+++ gulpfile.babel.js
@@ -0,0 +1,10 @@
+import config from './gulp/config';
+
+config.setEnv();
+
+export const build = () => {
+    console.log(config.isProd);
+}
+
+export const test = () => {
+    console.log(config.isProd
