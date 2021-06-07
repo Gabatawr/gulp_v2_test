09fddef5c42ae680edffa5074b9d9efa143208f4
Artem Gabatawr <gabatawr@gmail.com>
2021-06-07 13:29:31 +0300

    pug scss

---
+++ gulp/tasks/pug.js
@@ -0,0 +1,45 @@
+import gulp from 'gulp';
+import gulpif from 'gulp-if';
+import plumber from 'gulp-plumber';
+import pug from 'gulp-pug';
+import { setup as emittySetup } from '@zoxon/emitty';
+import config from '../config';
+
+const emittyPub = emittySetup(
+  config.src.pug,
+  'pug',
+  { makeVinylFile: true },
+);
+
+global.watch = false;
+global.emittyChangedFile = {
+  path: '',
+  stats: null,
+};
+
+export const pugBuild = () => (
+  gulp.src(`${config.src.pug}/*.pug`)
+    .pipe(plumber())
+    .pipe(
+      gulpif(
+        global.watch,
+        emittyPub.stream(
+          global.emittyChangedFile.path,
+          global.emittyChangedFile.stats,
+        ),
+      ),
+    )
+    .pipe(pug({ pretty: true }))
+    .pipe(gulp.dest(`${config.dest.html}`))
+);
+
+export const pugWatch = () => {
+  global.watch = true;
+  gulp.watch(`${config.src.pug}/**/*.pug`, pugBuild)
+    .on('all', (event, filepath, stats) => {
+      global.emittyChangedFile = {
+        path: filepath,
+        stats,
+      };
+    });
+};
--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -3,16 +3,26 @@ import config from './gulp/config';
 import clean from './gulp/tasks/clean';
 import server from './gulp/tasks/server';
 import { scriptsBuild, scriptsWatch } from './gulp/tasks/scripts';
+import { pugBuild, pugWatch } from './gulp/tasks/pug';
+import { sassBuild, sassWatch } from './gulp/tasks/styles';
 
 config.setEnv();
 
 export const build = () => gulp.series(
   clean,
-  scriptsBuild,
+  gulp.parallel(
+    scriptsBuild,
+    pugBuild,
+    sassBuild,
+  ),
 );
 
 export const watch = () => gulp.series(
   build,
   server,
-  scriptsWatch,
+  gulp.parallel(
+    scriptsWatch,
+    pugWatch,
+    sassWatch,
+  ),
 );
