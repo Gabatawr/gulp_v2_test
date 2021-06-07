a6fc8e1375368ed3a07b9aa4b5dac67901e4d513
Gabatawr <gabatawr@gmail.com>
2021-06-08 00:49:36 +0300

    save

--- gulp/tasks/styles.js
+++ gulp/tasks/styles.js
@@ -1,6 +1,6 @@
 import gulp from 'gulp';
 import sass from 'gulp-sass';
-// import sassGlob from 'gulp-sass-glob';
+import sassGlob from 'gulp-sass-glob';
 import cssImport from 'gulp-cssimport';
 import plumber from 'gulp-plumber';
 import autoprefixer from 'gulp-autoprefixer';
@@ -8,12 +8,16 @@ import gcmq from 'gulp-group-css-media-queries';
 import cleanCSS from 'gulp-clean-css';
 import rename from 'gulp-rename';
 import gulpif from 'gulp-if';
+import smartGrid from 'smart-grid';
+import importFresh from 'import-fresh';
 import config from '../config';
 
-export const stylesBuild = () => (
+const SMART_GRID_CONFIG_NAME = 'smart-grid-config.js';
+
+const sassBuild = () => (
   gulp.src(`${config.src.sass}/main.scss`, { sourcemaps: config.isDev })
     .pipe(plumber())
-    // .pipe(sassGlob())
+    .pipe(sassGlob())
     .pipe(sass())
     .pipe(cssImport({
       includePaths: ['./node_modules'],
@@ -25,4 +29,15 @@ export const stylesBuild = () => (
     .pipe(gulp.dest(config.dest.css, { sourcemaps: config.isDev }))
 );
 
-export const stylesWatch = () => gulp.watch(`${config.src.sass}/**/*.scss`, stylesBuild);
+const smartGridBuild = (callback) => {
+  const smartGridConfig = importFresh(`../../${SMART_GRID_CONFIG_NAME}`);
+  smartGrid(`${config.src.sass}/generated`, smartGridConfig);
+  callback();
+};
+
+export const stylesBuild = () => gulp.series(smartGridBuild, sassBuild);
+
+export const stylesWatch = () => {
+  gulp.watch(`${config.src.sass}/**/*.scss`, sassBuild);
+  gulp.watch(`./${SMART_GRID_CONFIG_NAME}`, smartGridBuild);
+};
